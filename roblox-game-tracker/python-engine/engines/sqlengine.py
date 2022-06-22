b24481e9a8aa5e20fb04acb2e09dbcb19e3e68ca
Tonanth <Tonanth@ubuntu>
2022-06-22 13:43:44 -0500

    Add documentation and have read_col return 1 dim list

--- roblox-game-tracker/python-engine/engines/sqlengine.py
+++ roblox-game-tracker/python-engine/engines/sqlengine.py
@@ -1,6 +1,7 @@
 import psycopg2
 from psycopg2 import sql
 import getpass
+from typing import List
 
 
 class SQLEngine:
@@ -29,14 +30,38 @@ class SQLEngine:
         self.table_name = table_name
 
     def read_col(self, col_name: str):
-        """Accepts a column name and will return all rows from column"""
+        """Accepts a column name and will return all data from column in a one dimensional list.
+
+        This is equivalent to the SQL command "SELECT col_name FROM table;".
+
+        Args:
+            col_name: The name of the column to get data from
+        Returns:
+            A one dimensional list containing all data from a given col_name.
+        """
         query = sql.SQL("select {field} from {table}").format(field=sql.Identifier(col_name),
                                                               table=sql.Identifier(self.table_name))
         self.cur.execute(query)
-        return self.cur.fetchall()
+        rows = self.cur.fetchall()
+        rows_squashed = [row[0] for row in rows]
+        return rows_squashed
 
-    def update_row(self, col_names, row, primary_col, primary_value):
-        """Updates database using primary_col to target desired row"""
+    def update_row(self,
+                   col_names: List[str], row, primary_col, primary_value):
+        """Updates database using primary_col to target desired row.
+
+        Equivalent to the SQL command "UPDATE table SET col_name[i] = row[i] WHERE primary_col = primary_value" for
+        i in len(row).
+
+        Args:
+            col_names: A list of column names to update in the database table.
+            row: A list of values to update corresponding to each column name in col_names.
+            primary_col: The primary column name
+            primary_value: The primary value to target the row
+
+        Returns:
+            None
+        """
         if len(col_names) != len(row):
             raise ValueError("col_names and row must have the same length")
         for col_name, value in zip(col_names, row):
@@ -49,8 +74,15 @@ class SQLEngine:
         return
 
     def rollback(self):
-        """Rollback transactions when a bad SQL call has been placed"""
+        """Rollback transactions when a bad SQL call has been placed
+
+        When a bad SQL query has been issued, no further SQL commands will be accepted until rollback() is called.
+
+        Returns:
+            None
+        """
         self.conn.rollback()
+        return
 
     def close(self):
         """Closes the connection to the database"""
