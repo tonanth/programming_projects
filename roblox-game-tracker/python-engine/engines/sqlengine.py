fc69ffbb19194735abc36fe2933100f774ad716e
Tonanth <Tonanth@ubuntu>
2022-06-22 02:06:59 -0500

    Finish implementing functions

--- roblox-game-tracker/python-engine/engines/sqlengine.py
+++ roblox-game-tracker/python-engine/engines/sqlengine.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import sql
 import getpass
 
 
@@ -12,8 +13,9 @@ class SQLEngine:
     """
 
     def __init__(self,
+                 table_name,
                  database_name: str,
-                 database_user: None):
+                 database_user: str = None):
         """Initializes connection to Postgres and saves connection object.
             Args:
                 database_name: The name of the database to use.
@@ -24,19 +26,31 @@ class SQLEngine:
         database_user = getpass.getuser() if database_user is None else database_user
         self.conn = psycopg2.connect(f'dbname={database_name} user={database_user}')
         self.cur = self.conn.cursor()
+        self.table_name = table_name
 
     def read_col(self, col_name: str):
         """Accepts a column name and will return all rows from column"""
-        self.cur.execute("")
-        self.conn.commit()
+        query = sql.SQL("select {field} from {table}").format(field=sql.Identifier(col_name),
+                                                              table=sql.Identifier(self.table_name))
+        self.cur.execute(query)
         return self.cur.fetchall()
 
-    def update_table(self, col_names, rows, primary_row):
-        """Updates database using primary_row to target desired rows"""
-
-        self.cur.execute()
+    def update_row(self, col_names, row, primary_col, primary_value):
+        """Updates database using primary_col to target desired row"""
+        if len(col_names) != len(row):
+            raise ValueError("col_names and row must have the same length")
+        for col_name, value in zip(col_names, row):
+            query = sql.SQL('update {table} set {field} = %s where {target} = %s').format(
+                table=sql.Identifier(self.table_name),
+                field=sql.Identifier(col_name),
+                target=sql.Identifier(primary_col))
+            self.cur.execute(query, (value, primary_value))
         self.conn.commit()
         return
+
+    def rollback(self):
+        """Rollback transactions when a bad SQL call has been placed"""
+        self.conn.rollback()
 
     def close(self):
         """Closes the connection to the database"""
