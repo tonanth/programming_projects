3c215c5ac917c5f8321bebc12010a02011cb0d89
Tonanth <Tonanth@ubuntu>
2022-06-22 15:14:29 -0500

    Add functional test main.py

--- roblox-game-tracker/python-engine/main.py
+++ roblox-game-tracker/python-engine/main.py
@@ -1,21 +1,10 @@
-from engine import CrawlerEngine, SQLEngine
-from config import attribute_xpath_dict
 import time
+from driver import Driver
 
-page_delay = 10             # Time delay in seconds between each game page load to avoid rate limiting
-loop_infinitely = False     # Check games continuously instead of just a single pass
+driver = Driver('test', 'test2', 'url', ['name', 'date_updated', 'description'])
 
-class Driver:
-    def __init__(self):
-        self.crawler = CrawlerEngine()
-        self.sql = SQLEngine()
+while True:
+    driver.iterate()
+    time.sleep(10)
 
-    # TODO finish implementing
-    def get_test_game(self):
-        self.driver.get('https://google.com')
-        print('implement this')
-        # Have the driver test out a sample game place
-
-    # Get table of games
-    def get_game_table(self):
-        table = self.sql.
+driver.close()
